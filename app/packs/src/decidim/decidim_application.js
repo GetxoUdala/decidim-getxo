76d7be003b9b1c8e0135f3b9cbc4d5c84456363b
Ivan Vergés <ivan@pokecode.net>
2023-02-06 13:42:56 +0100

    add missing js

--- app/packs/src/decidim/decidim_application.js
+++ app/packs/src/decidim/decidim_application.js
@@ -1,6 +1,8 @@
 // This file is compiled inside Decidim core pack. Code can be added here and will be executed
 // as part of that pack
 
+import "../getxo/disable_vote_button_budgets";
+
 // Load images
 require.context("../../images", true)
 // Load fonts
---
+++ app/packs/src/getxo/disable_vote_button_budgets.js
@@ -0,0 +1,5 @@
+$(() => {
+  $("#projects .budget-list__item .budget-list__data form.button_to").on("submit", function () {
+    $(".getxo-budget-vote-button").attr("disabled", true);
+  });
+});
