0b3dec15d12dbbef956b6dafaf56ed4756e0e791
Ivan Vergés <ivan@pokecode.net>
2023-02-28 20:24:35 +0100

    migrate to 0.26 & add signup module (#59)
    * init migration to 0.26
    
    * linting
    
    * fix select2
    
    * fix workflow
    
    * fix workflow
    
    * add sidekiq entries
    
    * add recapcha deface views
    
    * fix overrides
    
    * add missing js
    
    * gitignore
    
    * workflow
    
    * bundle
    
    * update bundler
    
    * add module signup
    
    * fix module signup
    
    * gemlock
    
    * linting
    
    * update decidim & plugins
    
    * workflows

--- app/controllers/concerns/decidim/admin/needs_multiselect_snippets.rb
+++ app/controllers/concerns/decidim/admin/needs_multiselect_snippets.rb
@@ -15,9 +15,9 @@ module Decidim
         return @snippets if @snippets
 
         @snippets = Decidim::Snippets.new
-
-        @snippets.add(:select2, ActionController::Base.helpers.stylesheet_link_tag("select2.css"))
-        @snippets.add(:select2, ActionController::Base.helpers.javascript_include_tag("resource_permissions_multiselect"))
+        @snippets.add(:select2, ActionController::Base.helpers.stylesheet_link_tag("https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css"))
+        @snippets.add(:select2, ActionController::Base.helpers.javascript_include_tag("https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"))
+        @snippets.add(:select2, ActionController::Base.helpers.javascript_pack_tag("resource_permissions_multiselect"))
         @snippets.add(:head, @snippets.for(:select2))
 
         @snippets
