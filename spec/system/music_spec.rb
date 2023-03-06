b0d2480db07c8c476e46b2c60888d7d4b105ee6e
Anna Topalidi <t.opalidisanna@gmail.com>
2023-03-06 19:12:19 +0100

    add music

---
+++ spec/system/music_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Visit a proposal", type: :system do
+  let(:organization) { create(:organization) }
+  let!(:process) do
+    create(
+      :participatory_process, :active, :promoted, organization: organization,
+    )
+  end
+
+  before do
+    switch_to_host(organization.host)
+    visit decidim_participatory_processes.participatory_processes_path
+  end
+
+  describe "music player" do
+    it "plays the music when the page is loaded" do
+      within first(".card__content") do
+        find("h2.card__title").click
+      end
+
+      audio_element = find("audio#audio")
+
+      sleep 2
+      expect(audio_element[:paused]).to eq("false")
+      expect(audio_element[:loop]).to eq("true")
+    end
+  end
+end
