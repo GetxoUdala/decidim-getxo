6465cdab81b80762e2ffdf4144822c5cf9145d67
Ivan Vergés <ivan@pokecode.net>
2024-05-09 17:43:30 +0200

    remove autoplay (#87)
    * remove autoplay
    
    * add env for autoplay
    
    * tests
    
    * fix test

--- spec/system/music_spec.rb
+++ spec/system/music_spec.rb
@@ -6,8 +6,11 @@ describe "Visit a proposal", type: :system do
   let(:organization) { create(:organization) }
   let!(:process_old) { create(:participatory_process, :active, :promoted, organization: organization, created_at: "31.12.2022".to_date) }
   let!(:process_new) { create(:participatory_process, :active, :promoted, organization: organization, created_at: "01.01.2023".to_date) }
+  let(:autoplay) { "true" }
 
   before do
+    allow(ENV).to receive(:fetch).and_call_original
+    allow(ENV).to receive(:fetch).with("GETXO_AUTOPLAY", nil).and_return(autoplay)
     switch_to_host(organization.host)
     visit decidim_participatory_processes.participatory_processes_path
   end
@@ -28,11 +31,21 @@ describe "Visit a proposal", type: :system do
           find("h2.card__title", text: translated(process_new.title)).click
         end
 
-        audio_element = find("audio#audio")
+        find("body").click
+        expect(page.execute_script("return document.getElementById('audio').paused")).to be(false)
+      end
+    end
 
-        sleep 2
-        expect(audio_element[:paused]).to eq("false")
-        expect(audio_element[:loop]).to eq("true")
+    context "when autoplay is disabled" do
+      let(:autoplay) { "false" }
+
+      it "doesn't play the music when the page is loaded" do
+        within first("#highlighted-processes") do
+          find("h2.card__title", text: translated(process_new.title)).click
+        end
+
+        find("body").click
+        expect(page.execute_script("return document.getElementById('audio').paused")).to be(true)
       end
     end
   end
