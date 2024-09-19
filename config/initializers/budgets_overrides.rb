c57708d5d7dee5ac8bb36ffb0e62b19690ec58ea
Ivan Vergés <ivan@pokecode.net>
2024-09-19 18:53:57 +0200

    make custom budgets optional

--- config/initializers/budgets_overrides.rb
+++ config/initializers/budgets_overrides.rb
@@ -1,25 +1,37 @@
 # frozen_string_literal: true
 
+Decidim.component_registry.find(:budgets).tap do |component|
+  component.settings(:global) do |settings|
+    settings.attribute :disable_custom_budgets, type: :boolean, default: false
+  end
+end
+
 Rails.application.config.to_prepare do
   Decidim::Budgets::Project.class_eval do
+    alias_method :original_confirmed_orders_count, :confirmed_orders_count
+
     def confirmed_votes_count
       query = <<-SQL.squish
-        SELECT SUM(score) FROM (
-          SELECT dbli.*, #{budget.projects.count} - (RANK() OVER (
-            PARTITION BY dbli.decidim_order_id
-            ORDER BY dbli.id ASC
+      SELECT SUM(score) FROM (
+        SELECT dbli.*, #{budget.projects.count} - (RANK() OVER (
+          PARTITION BY dbli.decidim_order_id
+          ORDER BY dbli.id ASC
           ) - 1) score
           FROM decidim_budgets_orders dbo
           JOIN decidim_budgets_line_items dbli ON dbo.id = dbli.decidim_order_id AND dbo.checked_out_at IS NOT NULL
           JOIN decidim_budgets_projects dbp ON dbp.id = dbli.decidim_project_id
-        ) ranks
-        WHERE decidim_project_id = #{id}
+          ) ranks
+          WHERE decidim_project_id = #{id}
       SQL
       @confirmed_votes_count ||= ActiveRecord::Base.connection.execute(Arel.sql(query))[0]["sum"].to_i
     end
 
     def confirmed_orders_count
-      confirmed_votes_count
+      if budget.component.settings.disable_custom_budgets
+        original_confirmed_orders_count
+      else
+        confirmed_votes_count
+      end
     end
   end
 
@@ -56,6 +68,8 @@ Rails.application.config.to_prepare do
   Decidim::Budgets::ProjectVotesCountCell.class_eval do
     delegate :current_order, to: :controller
 
+    alias_method :original_content, :content
+
     def votes_count
       @votes_count ||= if current_order.checked_out?
                          model.confirmed_orders_count
@@ -65,6 +79,8 @@ Rails.application.config.to_prepare do
     end
 
     def content
+      return original_content if model.budget.component.settings.disable_custom_budgets
+
       if options[:layout] == :one_line
         safe_join([votes_count, " ", label(t("decidim.budgets.projects.project.votes", count: votes_count))])
       else
@@ -78,9 +94,13 @@ Rails.application.config.to_prepare do
   end
 
   Decidim::Budgets::ProjectVotedHintCell.class_eval do
+    alias_method :original_hint, :hint
+
     private
 
     def hint
+      return original_hint if model.budget.component.settings.disable_custom_budgets
+
       contents = []
       contents << icon("check", role: "img", "aria-hidden": true)
       contents << " "
