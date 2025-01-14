97e73ffda5abc11b7057093e88115ede73277154
Anna Topalidi <t.opalidisanna@gmail.com>
2025-01-14 17:03:06 +0100

    lint

--- app/packs/entrypoints/resource_permissions_multiselect.js
+++ app/packs/entrypoints/resource_permissions_multiselect.js
@@ -1,28 +1,29 @@
 import "stylesheets/vendor/select2-foundation-theme.scss";
 
 $(() => {
+
   /**
    * Used to override simple inputs in the resource permissions controller
    * Allows to use more than one zone when configuring :census_authorization_handler
    * */
-  const url_zones = "/admin/getxo/zones";
+  const urlZones = "/admin/getxo/zones";
   
   const select2InputTags = (queryStr) => {
     const $input = $(queryStr)
 
-    const $select = $('<select class="'+ $input.attr('class') + '" style="width:100%" multiple="multiple"><select>');
-    if ($input.val() != "") {
-      const values = $input.val().split(',');
+    const $select = $(`<select class="${$input.attr("class")}" style="width:100%" multiple="multiple"><select>`);
+    if ($input.val() !== "") {
+      const values = $input.val().split(",");
       values.forEach((item) =>  {
-        $select.append('<option value="' + item + '" selected="selected">' + item + '</option>')
+        $select.append(`<option value="${item}" selected="selected">${item}</option>`)
       })
       ;
       // load text via ajax
-      $.get(url_zones, { ids: values }, (data) => {
+      $.get(urlZones, { ids: values }, (data) => {
         $select.val("");
         $select.contents("option").remove()
         data.forEach((item) => {
-         $select.append(new Option(item.text, item.id, true, true));
+          $select.append(new Option(item.text, item.id, true, true));
         });
         $select.trigger("change");
       }, "json");
@@ -40,7 +41,7 @@ $(() => {
   $('input[name$="[authorization_handlers_options][census_authorization_handler][zones]"]').each((idx, input) => {
     select2InputTags(input).select2({
       ajax: {
-        url: url_zones,
+        url: urlZones,
         delay: 100,
         dataType: "json",
         processResults: (data) => {
