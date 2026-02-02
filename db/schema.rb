a2af0bf7cd92d0ec0a11e1a36abb2862f7fd7767
Ivan Vergés <ivan@pokecode.net>
2026-02-02 20:31:25 +0100

    update decidim

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_17_132150) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_02_192852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
   enable_extension "pg_trgm"
@@ -1211,6 +1211,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_17_132150) do
     t.boolean "enable_participatory_space_filters", default: true
     t.jsonb "content_security_policy", default: {}
     t.jsonb "name", default: {}, null: false
+    t.jsonb "short_name", default: {}, null: false
     t.index ["host"], name: "index_decidim_organizations_on_host", unique: true
   end
 
