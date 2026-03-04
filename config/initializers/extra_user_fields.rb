9fc64fc4cc50cc4af1dbce7c1f2f4cc4523bb763
Ivan Vergés <ivan@pokecode.net>
2026-03-04 20:40:52 +0100

    update plugins

--- config/initializers/extra_user_fields.rb
+++ config/initializers/extra_user_fields.rb
@@ -1,3 +1,5 @@
 Decidim::ExtraUserFields.configure do |config|
-  config.insight_fields = %w(gender date_of_birth)
+  config.insight_fields = %w(gender age_span)
+  config.genders = [:female, :male]
+  config.insight_age_spans = ["16-17", "18-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75+"]
 end
